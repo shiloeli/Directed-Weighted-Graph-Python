eb0d3d66038dcd89889cc61353003f55e346aa3e
Lior <lior4007@gmail.com>
2021-01-11 14:28:03 +0200

    New test

--- test/GraphAlgoTest.py
+++ test/GraphAlgoTest.py
@@ -26,7 +26,7 @@ def test_graph_loaded(graph: str) -> None:
     g_algo = GraphAlgo()
     file = "../data/" + graph
     g_algo.load_from_json(file)
-    print(g_algo.get_graph())
+    # print(g_algo.get_graph())
     g_algo.save_to_json(file + "_edited")
     for k in g_algo.get_graph().get_all_v():
         for j in g_algo.get_graph().get_all_v():
@@ -180,35 +180,63 @@ class GraphAlgoTest(unittest.TestCase):
         file4 = "../data/A4"
         file5 = "../data/A5"
         fileT0 = "../data/T0.json"
+
         fileG_10 = "../data/G_10_80_0.json"
         fileG_100 = "../data/G_100_800_0.json"
         fileG_1000 = "../data/G_1000_8000_0.json"
         fileG_10000 = "../data/G_10000_80000_0.json"
         fileG_20000 = "../data/G_20000_160000_0.json"
         fileG_30000 = "../data/G_30000_240000_0.json"
-        # self.assertTrue(g.load_from_json(file0))
-        # g.plot_graph()
-        # self.assertTrue(g.load_from_json(file1))
-        # g.plot_graph()
-        # self.assertTrue(g.load_from_json(file2))
-        # g.plot_graph()
-        # self.assertTrue(g.load_from_json(file3))
-        # g.plot_graph()
-        # self.assertTrue(g.load_from_json(file4))
-        # g.plot_graph()
-        # self.assertTrue(g.load_from_json(file5))
-        # g.plot_graph()
-        # self.assertTrue(g.load_from_json(fileT0))
-        # g.plot_graph()
+
+        fileG_10_circle = "../data/G_10_80_1.json"
+        fileG_100_circle = "../data/G_100_800_1.json"
+        fileG_1000_circle = "../data/G_1000_8000_1.json"
+        fileG_10000_circle = "../data/G_10000_80000_1.json"
+        fileG_20000_circle = "../data/G_20000_160000_1.json"
+        fileG_30000_circle = "../data/G_30000_240000_1.json"
+
+        self.assertTrue(g.load_from_json(file0))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file1))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file2))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file3))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file4))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(file5))
+        g.plot_graph()
+        self.assertTrue(g.load_from_json(fileT0))
+        g.plot_graph()
+
         # self.assertTrue(g.load_from_json(fileG_10))
         # g.plot_graph()
         # self.assertTrue(g.load_from_json(fileG_100))
         # g.plot_graph()
-        self.assertTrue(g.load_from_json(fileG_1000))
-        g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_1000))
+        # g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_10000))
+        # g.plot_graph()
 
+        # self.assertTrue(g.load_from_json(fileG_10_circle))
+        # g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_100_circle))
+        # g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_1000_circle))
+        # g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_10000_circle))
+        # g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_20000_circle))
+        # g.plot_graph()
+        # self.assertTrue(g.load_from_json(fileG_30000_circle))
+        # g.plot_graph()
 
     def test_all_shortest_path(self):
+        """
+        Calculate a shortest path from each node to the other nodes
+        :return: None
+        """
         test_graph_loaded("A0")
         test_graph_loaded("A1")
         test_graph_loaded("A2")
@@ -216,6 +244,34 @@ class GraphAlgoTest(unittest.TestCase):
         test_graph_loaded("A4")
         test_graph_loaded("A5")
         test_graph_loaded("T0.json")
+
+        test_graph_loaded("G_10_80_0.json")
+        test_graph_loaded("G_100_800_0.json")
+        test_graph_loaded("G_1000_8000_0.json")
+        test_graph_loaded("G_10000_80000_0.json")
+        test_graph_loaded("G_20000_160000_0.json")
+        test_graph_loaded("G_30000_240000_0.json")
+
+        test_graph_loaded("G_10_80_1.json")
+        test_graph_loaded("G_100_800_1.json")
+        test_graph_loaded("G_1000_8000_1.json")
+        test_graph_loaded("G_10000_80000_1.json")
+        test_graph_loaded("G_20000_160000_1.json")
+        test_graph_loaded("G_30000_240000_1.json")
+
+    def test_connected_component_in_graph_loaded(self):
+        ga = GraphAlgo()
+        ga.load_from_json("../data/G_10_80_0.json")
+        component = ga.connected_component(0)
+        self.assertEqual(len(component), ga.get_graph().v_size())
+
+        ga.load_from_json("../data/G_100_800_0.json")
+        component = ga.connected_component(0)
+        self.assertEqual(len(component), ga.get_graph().v_size())
+
+        ga.load_from_json("../data/G_1000_8000_0.json")
+        component = ga.connected_component(0)
+        self.assertEqual(len(component), ga.get_graph().v_size())
 
     def test_plot_graph_of_100_vertices(self):
         g = graph_creator(100, 100)
